e1577f88be61cb402064a0acc2c0d778969a220b
Carlos Aguilar Pérez <caguilarfox@mail.com>
2023-07-11 15:13:43 -0500

    Cambio de pruebs git en laptop acer

--- vistas/modulos/reportes.php
+++ vistas/modulos/reportes.php
@@ -11,7 +11,7 @@
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <h1>
-       Reportes General de Salidas
+       Reportes General de Salids
        
       </h1>
       <ol class="breadcrumb">
